94a90f6337339e16c2fe24bc04791db7f18edbcc
sagi770 <sagi.770@gmail.com>
2020-06-15 13:19:43 +0300

    Updates

---
+++ precache-manifest.fe9244cd5ddc513e7f288a8fc8d1db76.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f5e10b931fac65df4f5273393a447da",
+    "url": "/print-stickers-labels/index.html"
+  },
+  {
+    "revision": "382226c11fd04b764b19",
+    "url": "/print-stickers-labels/static/css/main.4b420472.chunk.css"
+  },
+  {
+    "revision": "b1a648b70058b8e2cfa6",
+    "url": "/print-stickers-labels/static/js/2.f08042b8.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/print-stickers-labels/static/js/2.f08042b8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "382226c11fd04b764b19",
+    "url": "/print-stickers-labels/static/js/main.45584cdb.chunk.js"
+  },
+  {
+    "revision": "53cfa7e862af551eb4a2",
+    "url": "/print-stickers-labels/static/js/runtime-main.173140d6.js"
+  }
+]);
